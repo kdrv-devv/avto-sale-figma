3da2b72936baff592942cd7f73ab82fae2fa7b80
kdrv dev <ffeodalik@gmail.com>
2024-10-30 07:05:54 +0500

    to be countined

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // src papkangiz ichidagi barcha HTML va JS fayllarni kuzatadi
+    "./index.html" // asosiy HTML faylingizni ham kuzatadi
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
